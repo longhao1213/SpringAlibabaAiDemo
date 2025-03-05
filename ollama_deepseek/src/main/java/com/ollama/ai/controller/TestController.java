f9b39743a3f75b6c98e50a63463eeb2fd4b25978
longhao <1281578910@qq.com>
2025-03-05 15:18:00 +0800

    添加ollama整合本地的deepseek模型

---
+++ ollama_deepseek/src/main/java/com/ollama/ai/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.ollama.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private final ChatClient chatClient;
+
+    public TestController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(String input) {
+        return this.chatClient.prompt()
+                .user(input)
+                .call()
+                .content();
+    }
+
+}
